d2762c246863322323ad49f681105b527ee3e3b3
Trần Nguyễn Thành Hiển <thanhhien.work.2004@gmail.com>
2025-06-21 15:37:54 +0700

    Them thu viện do  bạn Huy lười

--- prisma/migrations/20250606114042_up/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Doctors" DROP CONSTRAINT "Doctors_id_fkey";
-
--- AlterTable
-ALTER TABLE "Doctors" ALTER COLUMN "gender" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Doctors" ADD CONSTRAINT "Doctors_id_fkey" FOREIGN KEY ("id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/doctors/doctors.resolver.ts
+++ src/doctors/doctors.resolver.ts
@@ -1,9 +1,9 @@
 // doctors.resolver.ts
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { DoctorsService } from './doctors.service';
 import { Doctor as DoctorsGraphQL } from './model/doctors.model';
-// import { CreateDoctorDto, UpdateDoctorInput } from './dto/doctors.dto';
-// import { DoctorWithRelations } from './type/doctors.type';
+import { CreateDoctorDto, UpdateDoctorInput } from './dto/doctors.dto';
+import { DoctorWithRelations } from './type/doctors.type';
 import { AuthService } from '../auth/auth.service';
 
 @Resolver(() => DoctorsGraphQL)
@@ -13,28 +13,28 @@ export class DoctorsResolver {
     private authService: AuthService,
 ) {}
 
-  // @Query(() => [DoctorsGraphQL])
-  // async doctors(): Promise<DoctorWithRelations[]> {
-  //   return this.doctorsService.findAll();
-  // }
-  //
-  // @Query(() => DoctorsGraphQL, { nullable: true })
-  // async doctor(@Args('id') id: string): Promise<DoctorWithRelations> {
-  //   return this.doctorsService.findOne(id);
-  // }
-  //
-  // @Mutation(() => DoctorsGraphQL)
-  // async createDoctor(@Args('doctorData') doctorData: CreateDoctorDto): Promise<DoctorWithRelations> {
-  //   return this.doctorsService.create(doctorData);
-  // }
-  //
-  // @Mutation(() => DoctorsGraphQL)
-  // async deleteDoctor(@Args('id') id: string): Promise<DoctorWithRelations> {
-  //   return this.doctorsService.delete(id);
-  // }
-  //
-  // @Mutation(() => DoctorsGraphQL)
-  // async updateDoctor(@Args('id') id: string, @Args('doctorData') doctorData: UpdateDoctorInput): Promise<DoctorWithRelations> {
-  //   return this.doctorsService.update(id, doctorData);
-  // }
+  @Query(() => [DoctorsGraphQL])
+  async doctors(): Promise<DoctorWithRelations[]> {
+    return this.doctorsService.findAll();
+  }
+
+  @Query(() => DoctorsGraphQL, { nullable: true })
+  async doctor(@Args('id') id: string): Promise<DoctorWithRelations> {
+    return this.doctorsService.findOne(id);
+  }
+
+  @Mutation(() => DoctorsGraphQL)
+  async createDoctor(@Args('doctorData') doctorData: CreateDoctorDto): Promise<DoctorWithRelations> {
+    return this.doctorsService.create(doctorData);
+  }
+
+  @Mutation(() => DoctorsGraphQL)
+  async deleteDoctor(@Args('id') id: string): Promise<DoctorWithRelations> {
+    return this.doctorsService.delete(id);
+  }
+
+  @Mutation(() => DoctorsGraphQL)
+  async updateDoctor(@Args('id') id: string, @Args('doctorData') doctorData: UpdateDoctorInput): Promise<DoctorWithRelations> {
+    return this.doctorsService.update(id, doctorData);
+  }
 }
